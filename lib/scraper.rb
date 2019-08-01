9616f65a99eee7945131f69f4ba07219ece744ea
Emily Lillie <emilylillie3@gmail.com>
2019-08-01 20:42:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
 
+# html = open("https://flatironschool.com/")
 
+# doc = Nokogiri::HTML(html)
+
+# doc.css(".site-header__hero__headline").text.strip
+
+# or instead of text.strip, can iterate over it like anything else like: 
+# courses = doc.css(".tout__label.heading.heading--level-4")
+ 
+# courses.each do |course|
+#   puts course.text.strip
+# end
 
